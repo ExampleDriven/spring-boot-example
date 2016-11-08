31835bfec0b5b943e24ee2c007f17e0b178cfcbc
Peter Szanto <git@szantocsalad.hu>
2016-11-08 17:07:15 +0100

    adding devtools example

---
+++ devtools-example/src/main/java/com/example/exampledriven/SpringBootDevtoolsExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.exampledriven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevtoolsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDevtoolsExampleApplication.class, args);
+	}
+}
