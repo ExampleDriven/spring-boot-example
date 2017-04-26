1f31f83c591ae1a3a03e34d11d0312f9c60c90cf
Peter Szanto <git@szantocsalad.hu>
2017-04-26 18:42:53 +0200

    cleanup logging

--- spring-boot-autoconfig-example/spring-boot-autoconfig-example-service/src/main/java/org/exampledriven/springboot/SpringBootExampleApplication.java
+++ spring-boot-autoconfig-example/spring-boot-autoconfig-example-service/src/main/java/org/exampledriven/springboot/SpringBootExampleApplication.java
@@ -1,19 +1,20 @@
 package org.exampledriven.springboot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class SpringBootExampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootExampleApplication.class, args);
+//        SpringApplication.run(SpringBootExampleApplication.class, args);
+        new SpringApplicationBuilder()
+                .sources(SpringBootExampleApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
     }
 
 //    If this bean is enabled it will overwrite the autoconfigured bean
